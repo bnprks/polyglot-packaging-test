0a45250ee92b2404ec25add844c5401431c364ac
Ben Parks <bnprks+git@gmail.com>
2023-07-14 13:20:21 -0700

    Setup R documentation site

--- r/R/polyglot.packaging.R
+++ r/R/polyglot.packaging.R
@@ -2,11 +2,16 @@
 #' @importFrom Rcpp evalCpp
 NULL
 
+#' Basic addition
+#' @param a,b integers to add
+#' @return Integer sum of inputs
 #' @export
 add_R <- function(a, b) {
     add_cpp(a, b)
 }
 
+#' Get a string from C++
+#' @return Greeting message from C++
 #' @export
 message_R <- function() {
     message_cpp()
---
+++ r/README.md
@@ -0,0 +1,4 @@
+# polyglot.packaging
+
+This is the R package from a test repository demonstrating a C++ library
+with both R and Python bindings
