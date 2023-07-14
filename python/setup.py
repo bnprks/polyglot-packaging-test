5135855ca83808191e0bbadaf9c6833b642fcf79
Ben Parks <bnprks+git@gmail.com>
2023-07-13 19:21:19 -0700

    Cleanup setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,20 +2,10 @@ import glob
 import os.path
 
 # Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 
 __version__ = "0.0.1"
-
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
-
-import os
-print("IN INSTALLATION SCRIPT")
-print("pwd: ", os.getcwd())
-print("ls ..:", os.listdir(os.path.dirname(os.getcwd())))
-print("END OF INSTALLATION SCRIPT")
 
 # Handle improperly checked-out symlinks from git.
 # This happens for certain Windows users depending on git config
@@ -27,6 +17,7 @@ if not os.path.isdir("src/cpp-core"):
 
 ext_modules = [
     Pybind11Extension("polyglotpackaging.cpp",
+        # Sort input files to ensure reproducible builds (https://github.com/pybind/python_example/pull/53)
         sorted(
             ["src/main.cpp"] +
             glob.glob("src/cpp-core/*.cpp"),
@@ -41,7 +32,5 @@ setup(
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
     version = __version__
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
 )
 
